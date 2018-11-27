b13fd190d4084aa0a82e3de6361be3f7583d7ae0
tim <tim@luminosuslight.de>
2018-11-27 12:25:52 +0100

    Fix PEP8 warnings cache_patch_labels.py

--- server/cache_patch_labels.py
+++ server/cache_patch_labels.py
@@ -9,21 +9,26 @@ def process_line(line):
     return image_name, label
 
 
+def cache_patch_labels():
+
+    with open(args.labels_path, 'r') as f:
+        image_list = map(process_line, f.readlines())
+
+    cache = {}
+    for image_path, label in image_list:
+        _, image_name = os.path.split(image_path)
+        cache[image_name] = label
+
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+    with open(os.path.join(args.output_dir, args.output_file), 'w') as f:
+        pickle.dump(cache, f)
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--labels_path', default='../data/ddsm_2class/val.txt')
 parser.add_argument('--output_dir', default='data/labels/')
 parser.add_argument('--output_file', default='val.pickle')
 args = parser.parse_args()
 
-with open(args.labels_path, 'r') as f:
-    image_list = map(process_line, f.readlines())
-
-cache = {}
-for image_path, label in image_list:
-    _, image_name = os.path.split(image_path)
-    cache[image_name] = label
-
-if not os.path.exists(args.output_dir):
-    os.makedirs(args.output_dir)
-with open(os.path.join(args.output_dir, args.output_file), 'w') as f:
-    pickle.dump(cache, f)
+cache_patch_labels()
