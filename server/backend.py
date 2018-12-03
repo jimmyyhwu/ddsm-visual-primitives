afad640436893338618e53cbb26bcc76249c76c6
tim <tim@luminosuslight.de>
2018-12-03 16:39:11 +0100

    Fix UI hint if patch is cancerous not working (++++----)

--- server/backend.py
+++ server/backend.py
@@ -57,7 +57,7 @@ def get_units(name, model, layer, sample=8, full=False, ranked=False):
         unit_dir = os.path.join(layer_dir, unit)
         image_names = sorted(os.listdir(os.path.join(STATIC_DIR, unit_dir)))[:sample]
         unit_labels = [labels[image_name[5:]] for image_name in image_names]
-        unit_labels_str = ' '.join(['+' if label == 1 else '-' for label in unit_labels])
+        unit_labels_str = ' '.join(['+' if label >= 1 else '-' for label in unit_labels])
         image_paths = [os.path.join(unit_dir, x) for x in image_names]
         units.append((unit, message, image_paths, unit_labels_str))
         sums.append(sample - sum(unit_labels))
