4d0f294912d31bb3f5b61f44224231eb273433cb
alpreu <11444089+alpreu@users.noreply.github.com>
2018-12-12 15:36:25 +0100

    refactoring db module (WIP due to performance issues

--- db/database.py
+++ db/database.py
@@ -1,6 +1,9 @@
 import sqlite3 as lite
 import os
 
+from populate_images import populate_db_with_images
+from populate_patches import populate_db_with_patches
+
 
 class DB:
     def __init__(self, filename, db_root="../db/"):
@@ -11,10 +14,10 @@ class DB:
         else:
             self.__conn = lite.connect(db_file_path)
             self.__generate_tables()
+            self.__populate_tables()
 
     def get_connection(self):
         return self.__conn
-
 
     def __generate_tables(self):
         with open(os.path.join(self._db_root, "init.sql"), "r") as generation_script:
@@ -22,3 +25,12 @@ class DB:
             self.__conn.commit()
             self.__conn.executescript(generation_script.read())
             self.__conn.commit()
+
+    def __populate_tables(self):
+        # populate images
+        image_list_path = os.path.join(self._db_root, "..", "data", "ddsm_raw_image_lists")
+        populate_db_with_images(self.__conn, image_list_path)
+        # populate patches
+        patch_path = os.path.join(self._db_root, "..", "data", "ddsm_patches")
+        patch_list_path = os.path.join(self._db_root, "..", "data", "ddsm_3class")
+        populate_db_with_patches(self.__conn, patch_path, patch_list_path)
