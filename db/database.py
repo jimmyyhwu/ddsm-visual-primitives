e00976837a82f434735d820b6bd317ee9ec04e49
tim <tim@luminosuslight.de>
2018-12-12 10:55:46 +0100

    Add environment variable to configure DB file path

--- db/database.py
+++ db/database.py
@@ -1,34 +1,26 @@
 import sqlite3 as lite
 import os
 
+
 class DB:
     def __init__(self, filename):
-        '''
-
-        :param filename:
-        :return:
-        '''
         if os.path.isfile(filename):
             self.__conn = lite.connect(filename)
         else:
             self.__conn = lite.connect(filename)
-            self.__generate_tables(self.__conn)
+            self.__generate_tables()
 
     def get_connection(self):
-        '''
-
-        :return:
-        '''
         return self.__conn
 
-    def __generate_tables(self, conn):
+    def __generate_tables(self):
         with open("init.sql", "r") as generation_script:
-            conn.execute("PRAGMA foreign_keys=on;")
-            conn.commit()
-            conn.executescript(generation_script.read())
-            conn.commit()
+            self.__conn.execute("PRAGMA foreign_keys=on;")
+            self.__conn.commit()
+            self.__conn.executescript(generation_script.read())
+            self.__conn.commit()
 
 
 if __name__ == "__main__":
-    DB_FILE = "test.db"
+    DB_FILE = os.environ['DB_FILE'] or 'test.db'
     db = DB(DB_FILE)
