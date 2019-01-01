fa94e09cace2bbf909704195666d8565c5944b3e
Jimmy Wu <jimmywu@alum.mit.edu>
2019-01-01 12:15:28 -0500

    Add DDSM mask labels

--- download_data.sh
+++ download_data.sh
@@ -3,7 +3,9 @@ wget http://data.csail.mit.edu/places/medical/data/ddsm_patches.tar.gz
 wget http://data.csail.mit.edu/places/medical/data/ddsm_labels.tar.gz
 wget http://data.csail.mit.edu/places/medical/data/ddsm_raw.tar.gz
 wget http://data.csail.mit.edu/places/medical/data/ddsm_raw_image_lists.tar.gz
+wget http://data.csail.mit.edu/places/medical/data/ddsm_masks.tar.gz
 tar -xf ddsm_patches.tar.gz
 tar -xf ddsm_labels.tar.gz
 tar -xf ddsm_raw.tar.gz
 tar -xf ddsm_raw_image_lists.tar.gz
+tar -xf ddsm_masks.tar.gz
