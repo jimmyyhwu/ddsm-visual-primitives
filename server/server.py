d6e4c6765afaf9711b1db6496c05bc2e902c40e2
tim <tim@luminosuslight.de>
2019-01-09 16:23:05 +0100

    Fix wrong method name

--- server/server.py
+++ server/server.py
@@ -188,7 +188,7 @@ def single_image():
 
 @app.route('/example_analysis')
 def example_analysis():
-    example_analysis('cancer_05-C_0128_1.LEFT_CC.LJPEG.1.jpg')
+    image('cancer_05-C_0128_1.LEFT_CC.LJPEG.1.jpg')
 
 
 @app.route('/image/<image_path>')
