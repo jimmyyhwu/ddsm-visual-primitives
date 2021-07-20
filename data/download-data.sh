406348d2fccd497d91cb169559d92845a0fda6e6
Jimmy Wu <jimmywu@alum.mit.edu>
2021-07-19 20:07:14 -0700

    Update data directory

--- data/download-data.sh
+++ data/download-data.sh
@@ -1,11 +1,13 @@
-cd data
+echo Downloading data...
 wget http://data.csail.mit.edu/places/medical/data/ddsm_patches.tar.gz
 wget http://data.csail.mit.edu/places/medical/data/ddsm_labels.tar.gz
 wget http://data.csail.mit.edu/places/medical/data/ddsm_raw.tar.gz
-wget http://data.csail.mit.edu/places/medical/data/ddsm_raw_image_lists.tar.gz
 wget http://data.csail.mit.edu/places/medical/data/ddsm_masks.tar.gz
+#wget http://data.csail.mit.edu/places/medical/data/ddsm_raw_image_lists.tar.gz
+
+echo Unpacking data...
 tar -xf ddsm_patches.tar.gz
 tar -xf ddsm_labels.tar.gz
 tar -xf ddsm_raw.tar.gz
-tar -xf ddsm_raw_image_lists.tar.gz
 tar -xf ddsm_masks.tar.gz
+#tar -xf ddsm_raw_image_lists.tar.gz
