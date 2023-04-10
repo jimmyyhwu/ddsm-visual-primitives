e3dc238c6d00e03b90a1d69f1d634913ca14bd77
Jimmy Wu <jimmyyhwu@users.noreply.github.com>
2023-04-10 12:59:00 -0700

    Update download URLs

--- data/download-data.sh
+++ data/download-data.sh
@@ -1,9 +1,14 @@
 echo Downloading data...
-wget http://data.csail.mit.edu/places/medical/data/ddsm_patches.tar.gz
-wget http://data.csail.mit.edu/places/medical/data/ddsm_labels.tar.gz
-wget http://data.csail.mit.edu/places/medical/data/ddsm_raw.tar.gz
-wget http://data.csail.mit.edu/places/medical/data/ddsm_masks.tar.gz
-#wget http://data.csail.mit.edu/places/medical/data/ddsm_raw_image_lists.tar.gz
+#wget -c http://data.csail.mit.edu/places/medical/data/ddsm_patches.tar.gz
+#wget -c http://data.csail.mit.edu/places/medical/data/ddsm_labels.tar.gz
+#wget -c http://data.csail.mit.edu/places/medical/data/ddsm_raw.tar.gz
+#wget -c http://data.csail.mit.edu/places/medical/data/ddsm_masks.tar.gz
+#wget -c http://data.csail.mit.edu/places/medical/data/ddsm_raw_image_lists.tar.gz
+wget -c https://www.dropbox.com/s/xattw5gkfd72b1j/ddsm_patches.tar.gz
+wget -c https://www.dropbox.com/s/766akqdm4jt0ksd/ddsm_labels.tar.gz
+wget -c https://www.dropbox.com/s/33jx5t21avd0n5j/ddsm_raw.tar.gz
+wget -c https://www.dropbox.com/s/x8l97k6rgihtg5x/ddsm_masks.tar.gz
+#wget -c https://www.dropbox.com/s/pde2ohme5jxcuy4/ddsm_raw_image_lists.tar.gz
 
 echo Unpacking data...
 tar -xf ddsm_patches.tar.gz
--- training/download-pretrained.sh
+++ training/download-pretrained.sh
@@ -1,6 +1,11 @@
 cd pretrained
-wget http://data.csail.mit.edu/places/medical/pretrained/2class/alexnet/checkpoint_00000045.pth.tar -P alexnet_2class
-wget http://data.csail.mit.edu/places/medical/pretrained/2class/vgg16/checkpoint_00000012.pth.tar -P vgg16_2class
-wget http://data.csail.mit.edu/places/medical/pretrained/2class/inception_v3/checkpoint_00000007.pth.tar -P inception_v3_2class
-wget http://data.csail.mit.edu/places/medical/pretrained/2class/resnet152/checkpoint_00000005.pth.tar -P resnet152_2class
-wget http://data.csail.mit.edu/places/medical/pretrained/3class/resnet152/checkpoint_00000005.pth.tar -P resnet152_3class
+#wget -c http://data.csail.mit.edu/places/medical/pretrained/2class/alexnet/checkpoint_00000045.pth.tar -P alexnet_2class
+#wget -c http://data.csail.mit.edu/places/medical/pretrained/2class/vgg16/checkpoint_00000012.pth.tar -P vgg16_2class
+#wget -c http://data.csail.mit.edu/places/medical/pretrained/2class/inception_v3/checkpoint_00000007.pth.tar -P inception_v3_2class
+#wget -c http://data.csail.mit.edu/places/medical/pretrained/2class/resnet152/checkpoint_00000005.pth.tar -P resnet152_2class
+#wget -c http://data.csail.mit.edu/places/medical/pretrained/3class/resnet152/checkpoint_00000005.pth.tar -P resnet152_3class
+wget -c http://data.csail.mit.edu/places/medical/pretrained/2class/alexnet/checkpoint_00000045.pth.tar -P alexnet_2class
+wget -c https://www.dropbox.com/s/q4y2fwip9tuhe1n/checkpoint_00000012.pth.tar -P vgg16_2class
+wget -c https://www.dropbox.com/s/5op93fsz689ls8a/checkpoint_00000007.pth.tar -P inception_v3_2class
+wget -c https://www.dropbox.com/s/a5ec9rlfcj8kjxd/checkpoint_00000005.pth.tar -P resnet152_2class
+wget -c https://www.dropbox.com/s/4yxfv32db6hoqca/checkpoint_00000005.pth.tar -P resnet152_3class
